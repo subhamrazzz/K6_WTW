46677e2d53f93293289302b5db92346a8cbe7434
Subham Raj <subham.raj@pointsbet.com>
2024-09-16 01:19:14 +0530

    MinorChanges_V2

--- src/scenarios.js
+++ src/scenarios.js
@@ -1,13 +1,13 @@
 export const scenarios = {
   simple_load: {
-    executor: "per-vu-iterations", // each VUs with complete defined number of iterations.
+    executor: "per-vu-iterations", // each VUs will complete defined number of iterations.
     vus: 1,
     iterations: 10,
     startTime: "2s",
   },
 
   ramping_load: {
-    executor: "ramping-vus", // users with ramp-up and ramp-down based on defined scenario
+    executor: "ramping-vus", // users will ramp-up and ramp-down based on defined scenario
     startVUs: 1,
     stages: [
       { duration: "10s", target: 5 },
@@ -16,10 +16,16 @@ export const scenarios = {
     ],
   },
 
-  stress_load: {
-    executor: "constant-vus", // contant number of VUs will keep running for defined duration
-    vus: 4,
-    duration: "20s",
+  spike_load: {
+    executor: "ramping-vus",
+    startVUs: 1, // Start with 1 virtual user
+    stages: [
+      { duration: "10s", target: 1 }, // Ramp up to 1 VU
+      { duration: "5s", target: 10 }, // Spike up to 10 VUs suddenly
+      { duration: "15s", target: 10 }, // stay at 10 VUs for 15 seconds
+      { duration: "10s", target: 1 }, // Ramp down to 1 VU
+      { duration: "10s", target: 0 }, // Ramp down to 0 VUs
+    ],
   },
 
   control_load: {
